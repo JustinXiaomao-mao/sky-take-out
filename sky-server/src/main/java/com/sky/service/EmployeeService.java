aa78dc1ad1bcfd18436a78b0e195bcaa3b1dbc19
JustinXiaomao <118606819+JustinXiaomao@users.noreply.github.com>
2025-04-14 11:29:03 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,9 +1,17 @@
 package com.sky.service;
 
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 
 public interface EmployeeService {
+
+    /**
+     * 新增员工
+     * @param employeeDTO
+     */
+    //抽象方法不能有方法主体
+    void save(EmployeeDTO employeeDTO);
 
     /**
      * 员工登录
